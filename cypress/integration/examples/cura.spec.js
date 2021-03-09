46ff636475f20b508b62b18820934f7b7c40324e
tiagocbarbosa <tiagocastrobarbosa@gmail.com>
2021-03-08 22:39:42 -0300

    Make Appointment logic added

--- cypress/integration/examples/cura.spec.js
+++ cypress/integration/examples/cura.spec.js
@@ -9,4 +9,13 @@ describe('Cura Make Appointment', () => {
         cy.get('#txt-password').type('ThisIsNotAPassword')
         cy.get('#btn-login').click()
     })
+
+    it('Make Appointment', () => {
+        cy.get('#combo_facility').select('Hongkong CURA Healthcare Center')
+        cy.get('#chk_hospotal_readmission').click()
+        cy.get('#radio_program_medicaid').click()
+        cy.get('#txt_visit_date').type('08/03/2021')
+        cy.get('#txt_comment').type('My first appointment')
+        cy.get('#btn-book-appointment').click()
+    })
 })

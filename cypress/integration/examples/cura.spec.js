1802fd4be255b5c1eae9c39576fcd52c3ed93765
tiagocbarbosa <tiagocastrobarbosa@gmail.com>
2021-03-08 22:28:16 -0300

    Make Appointment button logic added

--- cypress/integration/examples/cura.spec.js
+++ cypress/integration/examples/cura.spec.js
@@ -2,4 +2,8 @@ describe('Cura Make Appointment', () => {
     it('Visit the URL', () => {
         cy.visit('https://katalon-demo-cura.herokuapp.com/')
     })
+
+    it('Click on Make Appointment', () => {
+        cy.get('#btn-make-appointment').click()
+    })
 })
